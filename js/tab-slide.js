528855531e6b363914a3bac44a520c535b22d95d
Yash Lohar <77219427+YashLohar@users.noreply.github.com>
2021-04-05 16:37:30 +0530

    Update tab-slide.js

--- js/tab-slide.js
+++ js/tab-slide.js
@@ -1,21 +1,21 @@
 $(document).ready(function(){
 
   $(".Bookmarking").click(function(){
-      $(".box-1").slideToggle(450);
+      $(".box-1").fadeToggle(150);
 
       $(".box-2").css("display","none");
       $(".box-3").css("display","none");
   });
 
   $(".Speedy").click(function(){
-      $(".box-2").slideToggle(450);
+      $(".box-2").fadeToggle(150);
 
       $(".box-1").css("display","none");
       $(".box-3").css("display","none");
   });
 
   $(".Easy").click(function(){
-      $(".box-3").slideToggle(450);
+      $(".box-3").fadeToggle(150);
 
       $(".box-1").css("display","none");
       $(".box-2").css("display","none");
